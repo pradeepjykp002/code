b6c4fd874d7bc1570b4258adfafd0448cbdefa6d
pradeepjykp002 <83570814+pradeepjykp002@users.noreply.github.com>
2021-07-07 22:01:57 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,12 +13,13 @@ Name = "myvpc"
 }
 }
 
-data "aws_availability_zones" "available" {}
+# data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "myvpc_public_subnet" {
     vpc_id = "${aws_vpc.myvpc.id}"
     cidr_block = "${var.subnet_one_cidr}"
-    availability_zone = "${data.aws_availability_zones.availability_zones.names[0]}"
+    availability_zone = "${var.availability_zone}"
+#     availability_zone = "${data.aws_availability_zones.availability_zones.ap-southeast-2a[0]}"
     map_public_ip_on_launch = true
     tags = {
         Name = "myvpc_public_subnet"
