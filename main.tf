5862a3e3f8d2ccd550f1c076dffab150e1c53b0e
TonyJoseMDS <josetony720@gmail.com>
2021-07-08 09:48:08 +1000

    Test

--- main.tf
+++ main.tf
@@ -43,19 +43,20 @@ tags= {
 Name = "myvpc_internet_gateway"
 }
 }
+
 ## create public route table (assosiated with internet gateway)
-resource "aws_route_table" "myvpc_public_subnet_route_table" {
-vpc_id = "${aws_vpc.myvpc.id}"
-route {
-cidr_block = "${var.route_table_cidr}"
-gateway_id = "${aws_internet_gateway.myvpc_internet_gateway.id}"
+resource "aws_route_table" "myvpc_public_subnet_route_table" 
+    {
+        vpc_id = "${aws_vpc.myvpc.id}"
+        route 
+            {
+                cidr_block = "${var.route_table_cidr}"
+                gateway_id = "${aws_internet_gateway.myvpc_internet_gateway.id}"
+            }
+            tags = {
+                Name = "myvpc_public_subnet_route_table"
+            }
 }
-tags = {
-Name = "myvpc_public_subnet_route_table"
-}
-}
-
-
 
 ## create private subnet route table
 resource "aws_route_table" "myvpc_private_subnet_route_table" {
@@ -64,23 +65,31 @@ tags = {
 Name = "myvpc_private_subnet_route_table"
 }
 }
+
 ## create default route table
-resource "aws_default_route_table" "myvpc_main_route_table" {
-default_route_table_id = "${aws_vpc.myvpc.default_route_table_id}"
-tags = {
-Name = "myvpc_main_route_table"
-}
-}
+# resource "aws_default_route_table" "myvpc_main_route_table" {
+# default_route_table_id = "${aws_vpc.myvpc.default_route_table_id}"
+# tags = {
+# Name = "myvpc_main_route_table"
+# }
+# }
+
+
 ## associate public subnet with public route table
 resource "aws_route_table_association" "myvpc_public_subnet_route_table" {
 subnet_id = "${aws_subnet.myvpc_public_subnet.id}"
 route_table_id = "${aws_route_table.myvpc_public_subnet_route_table.id}"
 }
+
+
 ## associate private subnets with private route table
-resource "aws_route_table_association" "myvpc_private_subnet_one_route_table_assosiation" {
-subnet_id = "${aws_subnet.myvpc_private_subnet.id}"
-route_table_id = "${aws_route_table.myvpc_private_subnet_route_table.id}"
+resource "aws_route_table_association" "myvpc_private_subnet_one_route_table_assosiation" 
+    {
+        subnet_id = "${aws_subnet.myvpc_private_subnet.id}"
+        route_table_id = "${aws_route_table.myvpc_private_subnet_route_table.id}"
 }
+
+
 # resource "aws_route_table_association" "myvpc_private_subnet_two_route_table_assosiation" {
 # subnet_id = "${aws_subnet.myvpc_private_subnet_two.id}"
 # route_table_id = "${aws_route_table.myvpc_private_subnet_route_table.id}"
