7ae4015eee5ae9167139e900cd0ac40a316def49
pradeepjykp002 <83570814+pradeepjykp002@users.noreply.github.com>
2021-07-07 21:00:38 +1000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+variable "access_key" { default = "AKIAUQ4WWYCSHUFTLSMW" }
+variable "secret_key" { default = "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" }
+variable "region" { default = "Sydney" }
+variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "subnet_one_cidr" { default = "10.0.1.0/24" }
+variable "subnet_two_cidr" { default = ["10.0.2.0/24", "10.0.3.0/24"] }
+variable "route_table_cidr" { default = "0.0.0.0/0" }
+variable "host" {default = "aws_instance.my_web_instance.public_dns"}
+variable "web_ports" { default = ["22", "80", "443", "3306"] }
+variable "db_ports" { default = ["22", "3306"] }
+variable "images" {
+  type = "map"
+  default = {
+    "Sydney"      = "ami-0a443decce6d88dc2"
+  }
+}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+
+resource "aws_vpc" "myvpc" {
+cidr_block = "${var.vpc_cidr}"
+enable_dns_hostnames = true
+tags = {
+Name = "myvpc"
+}
+}
