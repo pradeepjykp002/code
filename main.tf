e7272779bc5d59e4bd00494a0b203072e18723fb
pradeepjykp002 <83570814+pradeepjykp002@users.noreply.github.com>
2021-07-07 21:08:12 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 variable "access_key" { default = "AKIAUQ4WWYCSHUFTLSMW" }
 variable "secret_key" { default = "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" }
-variable "region" { default = "Sydney" }
+variable "region" { default = "ap-southeast-2" }
 variable "vpc_cidr" { default = "10.0.0.0/16" }
 variable "subnet_one_cidr" { default = "10.0.1.0/24" }
 variable "subnet_two_cidr" { default = ["10.0.2.0/24", "10.0.3.0/24"] }
@@ -11,7 +11,7 @@ variable "db_ports" { default = ["22", "3306"] }
 variable "images" {
   type = "map"
   default = {
-    "Sydney"      = "ami-0a443decce6d88dc2"
+    "ap-southeast-2"      = "ami-0a443decce6d88dc2"
   }
 }
 
