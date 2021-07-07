0a171fa4cc10cb3cd1dcd1bc7829af0fd7f3b1e3
pradeepjykp002 <83570814+pradeepjykp002@users.noreply.github.com>
2021-07-07 22:08:29 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,11 +63,11 @@ subnet_id = "${aws_subnet.myvpc_public_subnet.id}"
 route_table_id = "${aws_route_table.myvpc_public_subnet_route_table.id}"
 }
 ## associate private subnets with private route table
-resource "aws_route_table_association" "myvpc_private_subnet_one_route_table_assosiation" {
-subnet_id = "${aws_subnet.myvpc_private_subnet_one.id}"
-route_table_id = "${aws_route_table.myvpc_private_subnet_route_table.id}"
-}
-resource "aws_route_table_association" "myvpc_private_subnet_two_route_table_assosiation" {
-subnet_id = "${aws_subnet.myvpc_private_subnet_two.id}"
-route_table_id = "${aws_route_table.myvpc_private_subnet_route_table.id}"
-}
+# resource "aws_route_table_association" "myvpc_private_subnet_one_route_table_assosiation" {
+# subnet_id = "${aws_subnet.myvpc_private_subnet_one.id}"
+# route_table_id = "${aws_route_table.myvpc_private_subnet_route_table.id}"
+# }
+# resource "aws_route_table_association" "myvpc_private_subnet_two_route_table_assosiation" {
+# subnet_id = "${aws_subnet.myvpc_private_subnet_two.id}"
+# route_table_id = "${aws_route_table.myvpc_private_subnet_route_table.id}"
+# }
