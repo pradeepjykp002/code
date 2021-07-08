c1b21f5cad0b7bdda62996fdaca7cc01aaf2b7bb
pradeepjykp002 <83570814+pradeepjykp002@users.noreply.github.com>
2021-07-09 04:19:19 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-variable "access_key" { default = "AKIAUQ4WWYCSHUFTLSMW" }
-variable "secret_key" { default = "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" }
+variable "access_key" { default = "" }
+variable "secret_key" { default = ""}
 variable "region" { default = "ap-southeast-2" }
 variable "vpc_cidr" { default = "10.0.0.0/16" }
 variable "subnet_one_cidr" { default = "10.0.1.0/24" }
@@ -8,5 +8,10 @@ variable "route_table_cidr" { default = "0.0.0.0/0" }
 variable "host" {default = "aws_instance.my_web_instance.public_dns"}
 variable "web_ports" { default = ["22", "80", "443", "3306"] }
 variable "db_ports" { default = ["22", "3306"] }
-variable "images" { default = "ami-0a443decce6d88dc2" }
 variable "availability_zone" { default = "" }
+variable "ansible_user" {
+  default = "ec2-user"
+}
+variable "private_key" {
+  default = "/d/code/instance_login.pem"
+}
